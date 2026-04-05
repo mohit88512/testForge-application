659bb6ee7b921008c370f6f46f5dcd46fb49f67f
Mohit <s.mohit3547@gmail.com>
2026-04-05 12:43:26 +0530

    navbar updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './component/navbar'
+import Navbar from "./component/Navbar"
 import Home from './component/Home'
 import About from './component/About'
 import Footer from './component/Footer'
